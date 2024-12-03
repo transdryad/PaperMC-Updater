51bdd6574c3ab75d43308ad069e8a784357bb6b2
Hazel Viswanath <viswanath.hazel@gmail.com>
2024-12-03 16:41:09 -0700

    Prevent installing multiple copies of the same plugin

--- install_plugin.py
+++ install_plugin.py
@@ -13,6 +13,9 @@ def install(name, provider, identifier):
         server_info = json.load(read_file)
     version = server_info["version"]
     plugin = {"name": name, "provider": provider, "id": identifier}
+    if plugin in server_info["plugins"]:
+        print("You may not install multiple copies of the same plugin!")
+        return
     server_info["plugins"].append(plugin)
     with open("server.json", "w") as write_file:
         json.dump(server_info, write_file, indent=4)
