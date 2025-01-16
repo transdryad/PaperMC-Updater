64b3a4ab54f9840c7403aba8b8596caca71fda05
Hazel <109479539+transdryad@users.noreply.github.com>
2025-01-16 16:20:13 -0700

    oops

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx_pdj_theme',
+    'sphinx.ext.intersphinx'
 ]
 
 intersphinx_mapping = {
